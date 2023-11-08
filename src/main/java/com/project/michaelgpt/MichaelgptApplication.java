5ee862aedfa5564cb7fe750ac4792d15d9af5454
Michael Gulson <msgulson@gmail.com>
2023-11-08 10:47:27 -0600

    initialize spring application

---
+++ src/main/java/com/project/michaelgpt/MichaelgptApplication.java
@@ -0,0 +1,13 @@
+package com.project.michaelgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichaelgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichaelgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/michaelgpt/MichaelgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.michaelgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichaelgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
