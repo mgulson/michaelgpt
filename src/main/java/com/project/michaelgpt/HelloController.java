7f357e8fcfff6d0e5f4437c46440ee7d5f90c03c
Michael Gulson <msgulson@gmail.com>
2023-11-08 11:34:15 -0600

    hello world

---
+++ src/main/java/com/project/michaelgpt/HelloController.java
@@ -0,0 +1,13 @@
+package com.project.michaelgpt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "michaelgpt is operational!!!";
+	}
+}
--- src/main/java/com/project/michaelgpt/MichaelgptApplication.java
+++ src/main/java/com/project/michaelgpt/MichaelgptApplication.java
@@ -2,6 +2,10 @@ package com.project.michaelgpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class MichaelgptApplication {
@@ -10,4 +14,8 @@ public class MichaelgptApplication {
 		SpringApplication.run(MichaelgptApplication.class, args);
 	}
 
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 }
